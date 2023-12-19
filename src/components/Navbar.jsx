a2263db6f45c6a7f4c76a9cc545f79bbd2e5133e
DevRys <eduardoreyes55@aragon.unam.mx>
2023-12-18 21:07:54 -0600

    update navbar\

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
       <h1 className="ml-6 font-semibold text-xl">Next Js</h1>
       <ul className="flex">
         {routes.map((route) => (
-          <li className="ml-4 bg-transparent rounded-lg px-2 py-2 hover:bg-[#202020] border-2 border-[#202020] text-sm">
+          <li key={route.name} className="ml-4 bg-transparent rounded-lg px-2 py-2 hover:bg-[#202020] border-2 border-[#202020] text-sm">
             <Link href={route.path}>{route.name}</Link>
           </li>
         ))}
